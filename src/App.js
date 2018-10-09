20bedd9f190b6580c26b851ab1db89c3037106cf
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-09 11:32:08 +0200

    Update App component; add the Todos component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import Todos from './components/Todos';
 
 class App extends Component {
+  state = {
+    todos: []
+  }
+
+  addTodo = todo => {
+    this.setState(state => ({
+      todos: [todo, ...state.todos]
+    }))
+  }
+
   render() {
     return (
       <section className="container">
         <h1 className='center'>Todo's</h1>
+        <Todos
+          todos={this.state.todos}
+          addTodo={this.addTodo}
+        />
       </section>
     )
   }
