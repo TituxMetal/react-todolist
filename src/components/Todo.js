88f9b916cd5ec593d4d0543aa13fdce9981d9057
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-10 10:30:09 +0200

    Update Todo compnent; add onClick event function on icon and on paragraph to toggle done a todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,14 +2,15 @@ import React from 'react'
 
 import './Todo.css'
 
-export default ({ todo }) => {
+export default ({ todo, toggleComplete }) => {
+  const toggleDone = () => toggleComplete(todo.id)
   const icon = todo.complete ?
-    <i className='material-icons circle transparent green-text'>done</i> :
-    <i className='material-icons circle transparent'>radio_button_unchecked</i>
+    <i onClick={toggleDone} className='material-icons circle transparent green-text'>done</i> :
+    <i onClick={toggleDone} className='material-icons circle transparent'>radio_button_unchecked</i>
   return (
     <li className='collection-item grey darken-3 avatar valign-wrapper complete' key={todo.id}>
       {icon}
-      <p className={todo.complete ? 'completed' : ''}>{todo.text}</p>
+      <p className={todo.complete ? 'completed' : ''} onClick={toggleDone}>{todo.text}</p>
     </li>
   )
 }
