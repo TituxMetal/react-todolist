4f51e5f7516660dc1d5e960e74a2a8111fb6f041
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-10 10:35:07 +0200

    Update Todos component; add toggleComplete function to props

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,10 +3,10 @@ import React, { Fragment } from 'react'
 import Todo from './Todo'
 import TodoForm from './TodoForm'
 
-export default ({ todos, addTodo }) => {
+export default ({ todos, addTodo, toggleComplete }) => {
   const todoList = todos.length > 0 ?
     todos.map(todo =>
-      <Todo key={todo.id} todo={todo} />
+      <Todo key={todo.id} todo={todo} toggleComplete={toggleComplete} />
     ) :
     <li className="collection-item grey darken-3 valign-wrapper">
       <p className='col s8 offset-s2 l6 offset-l3'>No todo!</p>
