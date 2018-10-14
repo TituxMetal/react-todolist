d3854b09d46094d7e7b00742740d60bb2eccc625
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-14 13:22:33 +0200

    Update Todo component; add the delete button and onClick handler

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import './Todo.css'
 
-export default ({ todo, toggleComplete }) => {
+export default ({ todo, toggleComplete, onDelete }) => {
   const toggleDone = () => toggleComplete(todo.id)
   const icon = todo.complete ?
     <i onClick={toggleDone} className='material-icons circle transparent green-text'>done</i> :
@@ -11,6 +11,11 @@ export default ({ todo, toggleComplete }) => {
     <li className='collection-item grey darken-3 avatar valign-wrapper complete' key={todo.id}>
       {icon}
       <p className={todo.complete ? 'completed' : ''} onClick={toggleDone}>{todo.text}</p>
+      <span className='secondary-content'>
+        <button onClick={() => onDelete(todo.id)} className="btn-floating btn-small red tooltip" data-tooltip='Delete todo'>
+          <i className='material-icons'>delete</i>
+        </button>
+      </span>
     </li>
   )
 }
