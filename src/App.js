13a708f77bca582c3160132ba1ae47446f605740
Desktop | Lang Guillaume <lang.guillaume@gmail.com>
2018-10-16 03:30:31 +0200

    Update App.js file; add toggleAllComplete state property, add a method to toggle complete/uncomplete todos and pass it to Todos component as props, reset toggleAllComplete state property to true in deleteAllCompletedTodos method

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Tooltip } from 'materialize-css/dist/js/materialize.min.js'
 class App extends Component {
   state = {
     todos: [],
-    todosToShow: 'all'
+    todosToShow: 'all',
+    toggleAllComplete: true
   }
 
   componentDidMount() {
@@ -22,9 +23,20 @@ class App extends Component {
     }))
   }
 
+  completeAllTodos = () => {
+    this.setState(state => ({
+      todos: state.todos.map(todo => ({
+        ...todo,
+        complete: state.toggleAllComplete
+      })),
+      toggleAllComplete: !state.toggleAllComplete
+    }))
+  }
+
   deleteAllCompletedTodos = () => {
     this.setState(state => ({
-      todos: state.todos.filter(todo => !todo.complete)
+      todos: state.todos.filter(todo => !todo.complete),
+      toggleAllComplete: true
     }))
   }
 
@@ -66,7 +78,9 @@ class App extends Component {
           addTodo={this.addTodo}
           handleDelete={this.handleDelete}
           handleDeleteAllCompleted={this.deleteAllCompletedTodos}
+          handleCompleteAllTodos={this.completeAllTodos}
           toggleComplete={this.toggleComplete}
+          toggleAllComplete={this.state.toggleAllComplete}
           filterTodos={this.updateTodoToShow}
         />
       </section>
